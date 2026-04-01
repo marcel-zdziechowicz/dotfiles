6eaac0a4c6c40d1814f0fd293e66a7ec2b0af7bf
Marcel Zdziechowicz <marcel.zdziechowiczm@gmail.com>
2026-04-01 23:24:06 +0200

    fix: wrong script path

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,12 @@
 set -euo pipefail
 
 chmod +x ./variables.sh
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${SOURCE_DIR}/variables.sh"
+
 chmod +x ./scripts/live-iso-conf.sh
 ./scripts/live-iso-conf.sh
 
-arch-chroot /mnt chmod +x "/home/${USERNAME}/dotfiles/install.sh"
+arch-chroot /mnt chmod +x "/home/${USERNAME}/dotfiles/scripts/install.sh"
 INSTALL_PATH="/home/${USERNAME}/dotfiles/scripts/install.sh"
 arch-chroot /mnt sudo -u "$USERNAME" "$INSTALL_PATH"
