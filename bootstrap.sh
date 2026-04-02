828865d43fc933b5670ae72c82d67b0e1236f34c
Marcel Zdziechowicz <marcel.zdziechowiczm@gmail.com>
2026-04-02 22:27:06 +0200

    fix: run user-conf.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,17 @@ source "${SOURCE_DIR}/variables.sh"
 chmod +x ./scripts/live-iso-conf.sh
 ./scripts/live-iso-conf.sh
 
-arch-chroot /mnt chmod +x "/home/${USERNAME}/dotfiles/scripts/install.sh"
+arch-chroot /mnt chmod +x "/home/${USERNAME}/dotfiles/scripts/*"
+
 INSTALL_PATH="/home/${USERNAME}/dotfiles/scripts/install.sh"
 arch-chroot /mnt sudo -u "$USERNAME" "$INSTALL_PATH"
 
+USERCONF_PATH="/home/${USERNAME}/dotfiles/scripts/user-conf.sh"
+arch-chroot /mnt sudo -u "$USERNAME" "$USERCONF_PATH"
+
+# SYSCONF_PATH="/home/${USERNAME}/dotfiles/scripts/sys-conf.sh"
+# arch-chroot /mnt sudo -u "$USERNAME" "$SYSCONF_PATH"
+
 # TODO: Remove NOPASSWD from /etc/sudoers
+# chmod +x ./scripts/cleanup.sh
+#./scripts/cleanup.sh
