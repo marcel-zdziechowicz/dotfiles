ee1b36128d7f403063781715c741f46d304c5f57
Marcel Zdziechowicz <marcel.zdziechowiczm@gmail.com>
2026-04-03 11:59:28 +0200

    fix: automatically accept every prompt from yay

--- scripts/install.sh
+++ scripts/install.sh
@@ -36,4 +36,4 @@ ARGS=(--noconfirm --answerdiff None
 	--answerclean None --useask
 	--mflags "--noconfirm --skippgpcheck"
 )
-yay -Sy "${AUR_PKGS[@]}" "${ARGS[@]}"
+yes | yay -Sy "${AUR_PKGS[@]}" "${ARGS[@]}"
