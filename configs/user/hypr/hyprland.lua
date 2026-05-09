c505da1e31b0644bbdc0d6ef7fb13615c359cf4f
marcel-zdziechowicz <112772290+marcel-zdziechowicz@users.noreply.github.com>
2026-05-09 23:13:32 +0200

    Document legacy configuration options in hyprland.lua
    Add comments for legacy configuration sources

--- configs/user/hypr/hyprland.lua
+++ configs/user/hypr/hyprland.lua
@@ -6,3 +6,13 @@ require 'lua.style'
 require 'lua.monitors'
 require 'lua.input'
 require 'lua.windowrules'
+
+-- Easy switch to legacy configuration
+-- source = legacy-config/colors.conf
+-- source = legacy-config/variables.conf
+-- source = legacy-config/autostart.conf
+-- source = legacy-config/keybinds.conf
+-- source = legacy-config/style.conf
+-- source = legacy-config/monitors.conf
+-- source = legacy-config/input.conf
+-- source = legacy-config/windowrules.conf
